de7470a7dbccc51b2395a77e297e1109d60e7dd3
Поздеев Роман <rip@m2.ru>
2024-04-11 17:35:15 +0300

    fix key mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,7 +5,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr><esc>")
 
 map({ "n" }, "<Esc>", "<cmd>nohlsearch<CR>")
 
